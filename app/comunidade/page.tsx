3ddbb8ef419efe10a7c001385c0b95bc9577c1fb
GannJobs <linksyamato@gmail.com>
2024-08-29 19:40:37 -0300

    OK Inicial + Start comunity

--- app/comunidade/page.tsx
+++ app/comunidade/page.tsx
@@ -1,11 +1,35 @@
-import Image from "next/image";
+import axios from 'axios';
 import styles from "./page.module.css";
 
-export default function Home() {
+const fetchRecords = async () => {
+  try {
+    const res = await axios.get('http://127.0.0.1:8000/RecordC/');
+    return res.data.Records;  // Assuming the API returns the structure you provided
+  } catch (error) {
+    console.error('Error fetching records:', error);
+    return [];
+  }
+};
+
+export default async function Home() {
+  const records = await fetchRecords();
+
   return (
     <main className={styles.main}>
-      <h1>Test</h1>
-      
+      <div className={styles.list}>
+        <ul>
+          {records.map((record, index) => (
+            <li key={index}>
+              <h3>Título: {record.title}</h3>
+              <p>Descrição: {record.description}</p>
+              <p>Data e Hora: {record.created_at}</p>
+              <a href={record.returned_arq} download>
+                Análise da IA
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
-  );  
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -18,41 +18,18 @@ export default function Home() {
             </div>
             <div className={styles.BoxTexts}>
                 <div className={styles.titles}>
-                    <h1>Lorem Ipslum</h1>
-                    <h1>Lorem Ipslum</h1>
-                    <h1>Lorem Ipslum</h1>
-                    <h1>Lorem Ipslum</h1>
+                    <h1>Bem vindo ao</h1>
+                    <h2>Aqua Portal</h2>
+                    <h3>Sua base de dados comunitária com auxilio de IA</h3>
                 </div>
                 <div className={styles.Texts}>
                     <p>
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
+                        Temos o intuito de ser a maior comuindade de laudos da região, para auxiliar todos os
+                        analistas, especialistas e as empresas do ramo da qualidade de água, que é necessária para a vida não é mesmo?
+                        Junte-se a nos também, submeta um laudo e contribua para melhores analises da sua região, você está salvando não 
+                        somente o planeta, mas vidas também, e cada ato conta, contribua, ou consuma é importante que você também seja auxiliado no que deseja!
                     </p>
                 </div>
-            </div>
-            <div className={styles.Texts}>
-                    <p>
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                    </p>
-                    <br />
-                    <p>
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                    </p>
-                    <br />
-                    <p>
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                        Lorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem IpslumLorem Ipslum
-                    </p>
             </div>
         </main>
     )
